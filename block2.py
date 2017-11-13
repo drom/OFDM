7b19306fb5c3271aa438e850259f510a7f37b0c7
devang <devang@devang.info>
2017-11-13 04:29:32 -0800

    more changes

--- block2.py
+++ block2.py
@@ -7,6 +7,8 @@ be the parameters. All of them are required to have default values!
 """
 
 import numpy as np
+import pmt
+from pmt import pmt_to_python
 import time
 import os
 from gnuradio import gr
@@ -16,26 +18,28 @@ from multiprocessing import Pool, TimeoutError
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
     """Embedded Python Block example - a simple multiply const"""
 
-
-    def __init__(self, example_param=1.0):  # only default arguments here
+    def __init__(self):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
             name='find max',   # will show up in GRC
             in_sig=[(np.float32,320), (np.float32,320)],
             out_sig=[np.float32]
+            # out_sig=[]
         )
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
-        self.example_param = example_param
+        self.message_port_register_out(pmt.to_pmt("out"))
 
     def work(self, input_items, output_items):
 
-        index_at_corr1 = np.argmax(np.float64(input_items[0][0]))
+        index_at_corr1 = np.argmax(input_items[0][0])
 
         val_at_corr = input_items[1][0][index_at_corr1]
-        print '{}:{}'.format(index_at_corr1, val_at_corr)
+        # print '{}:{}'.format(index_at_corr1, val_at_corr)
+
+        fcorr = 3906.25010348*val_at_corr
+        self.message_port_pub(pmt.to_pmt("out"), pmt.cons(pmt.intern("freq"), pmt.to_pmt(fcorr)))
 
         output_items[0][:] = np.float32(val_at_corr)
-
         return len(output_items[0])
